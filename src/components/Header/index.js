9c4a8dbb4e861cac8904e6447bbbe159ca0a0882
Gus Prado <contato@gusprado.com>
2019-12-16 11:12:44 -0300

    Added Profile Logout and Schedule functionalities

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,6 +8,8 @@ import logo from '~/assets/logo_purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -18,10 +21,13 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>NOME</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
               <img
-                src="https://api.adorable.io/avatars/50/abott@adorable.png"
+                src={
+                  profile.avatar ||
+                  'https://api.adorable.io/avatars/50/abott@adorable.png'
+                }
                 alt="Prestador"
               />
             </div>
